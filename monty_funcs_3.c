d0fd95df9d1dfa8dffccbabd5aa36ea091965e3c
gideonopa <gideonoparinde@gmail.com>
2022-07-17 01:44:16 +0100

    my commit

--- monty_funcs_3.c
+++ monty_funcs_3.c
@@ -1,7 +1,9 @@
 #include "monty.h"
 
 void monty_nop(stack_t **stack, unsigned int line_number);
+
 void monty_pchar(stack_t **stack, unsigned int line_number);
+
 void monty_pstr(stack_t **stack, unsigned int line_number);
 
 /**
