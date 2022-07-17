84040ab673d09a7d02a52a618b837e9bcd169272
gideonopa <gideonoparinde@gmail.com>
2022-07-17 01:43:15 +0100

    my commit

--- monty_funcs_2.c
+++ monty_funcs_2.c
@@ -1,9 +1,13 @@
 #include "monty.h"
 
 void monty_add(stack_t **stack, unsigned int line_number);
+
 void monty_sub(stack_t **stack, unsigned int line_number);
+
 void monty_div(stack_t **stack, unsigned int line_number);
+
 void monty_mul(stack_t **stack, unsigned int line_number);
+
 void monty_mod(stack_t **stack, unsigned int line_number);
 
 /**
